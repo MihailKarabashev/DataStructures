d2d1292e6cdbd048f55e0342c9f102f03033587c
mihail-karabashev <mihailkarabashev@gmail.com>
2022-05-03 16:00:48 +0300

    Binary Search Tree implementation of insert and print in order

--- 06.Heaps-BST-Lab/02.BinarySearchTree/BinarySearchTree.cs
+++ 06.Heaps-BST-Lab/02.BinarySearchTree/BinarySearchTree.cs
@@ -5,6 +5,22 @@
     public class BinarySearchTree<T> : IBinarySearchTree<T>
         where T : IComparable<T>
     {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; private set; }
+
+            public Node Left { get; set; }
+
+            public Node Right { get; set; }
+        }
+
+        private Node root;
+
         public BinarySearchTree() { }
 
         public bool Contains(T element)
@@ -14,17 +30,49 @@
 
         public void EachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+
+           this.EachInOrderRecursive(action, this.root);
+
         }
 
-        public void Insert(T element)
+        private void EachInOrderRecursive(Action<T> action, Node node)
         {
-            throw new NotImplementedException();
+            if (node is null)
+            {
+                return;
+            }
+
+            this.EachInOrderRecursive(action,node.Left);
+            action.Invoke(node.Value);
+            this.EachInOrderRecursive(action,node.Right);
         }
 
         public IBinarySearchTree<T> Search(T element)
         {
             throw new NotImplementedException();
+        }
+
+        public void Insert(T element)
+        {
+            this.root = this.InsertRecursive(element, this.root);
+        }
+
+        private Node InsertRecursive(T element, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(element);
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.InsertRecursive(element, node.Left);
+            }
+            else if(element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.InsertRecursive(element,node.Right);
+            }
+
+            return node;
         }
     }
 }
--- 06.Heaps-BST-Lab/Demo/Program.cs
+++ 06.Heaps-BST-Lab/Demo/Program.cs
@@ -5,20 +5,24 @@ using _02.BinarySearchTree;
 using _03.MaxHeap;
 
 
-var tree = new BinaryTree<int>(
-                       17,
-                       new BinaryTree<int>(
-                           9,
-                           new BinaryTree<int>(3, null, null),
-                           new BinaryTree<int>(11, null, null)
-                       ),
-                       new BinaryTree<int>(
-                           25,
-                           new BinaryTree<int>(20, null, null),
-                           new BinaryTree<int>(31, null, null)
-                       )
-       );
+//var tree = new BinaryTree<int>(
+//                       17,
+//                       new BinaryTree<int>(
+//                           9,
+//                           new BinaryTree<int>(3, null, null),
+//                           new BinaryTree<int>(11, null, null)
+//                       ),
+//                       new BinaryTree<int>(
+//                           25,
+//                           new BinaryTree<int>(20, null, null),
+//                           new BinaryTree<int>(31, null, null)
+//                       )
+//       );
 
+var tree = new BinarySearchTree<int>();
 
-tree.ForEachInOrder(x => Console.Write(string.Join(" ", x)));
+tree.Insert(8);
+tree.Insert(4);
+tree.Insert(2);
 
+tree.EachInOrder(Console.WriteLine);
