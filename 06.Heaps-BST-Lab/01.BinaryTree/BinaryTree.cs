8d567d24f8b92bd9441e84d6aec6c3bfa09de136
mihail-karabashev <mihailkarabashev@gmail.com>
2022-05-03 13:22:24 +0300

    Implement Binary Tree With Traversal Pre-Order

---
+++ 06.Heaps-BST-Lab/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,59 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            this.Value = element;
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> InOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
+        {
+            var list = new List<IAbstractBinaryTree<T>>();
+            list.Add(this);
+
+            if (this.LeftChild is not null)
+            {
+                list.AddRange(this.LeftChild.PreOrder());
+            }
+
+            if (this.RightChild is not null)
+            {
+                list.AddRange(this.RightChild.PreOrder());
+            }
+
+            return list;
+        }
+    }
+}
---
+++ 06.Heaps-BST-Lab/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using _01.BinaryTree;
+using _02.BinarySearchTree;
+using _03.MaxHeap;
+
+
+var tree = new BinaryTree<int>(
+                       17,
+                       new BinaryTree<int>(
+                           9,
+                           new BinaryTree<int>(3, null, null),
+                           new BinaryTree<int>(11, null, null)
+                       ),
+                       new BinaryTree<int>(
+                           25,
+                           new BinaryTree<int>(20, null, null),
+                           new BinaryTree<int>(31, null, null)
+                       )
+       );
+
+
+tree.PreOrder();
+;
