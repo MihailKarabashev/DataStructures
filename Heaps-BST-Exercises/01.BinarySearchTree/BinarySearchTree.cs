8828c1975636621e18816d812ff147552673ed07
mihail-karabashev <mihailkarabashev@gmail.com>
2022-05-05 16:27:25 +0300

    Add Heap and BST Excersises

---
+++ Heaps-BST-Exercises/01.BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,211 @@
+﻿namespace _02.BinarySearchTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class BinarySearchTree<T> : IBinarySearchTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        private Node root;
+
+        private BinarySearchTree(Node node)
+        {
+            this.PreOrderCopy(node);
+        }
+
+        public BinarySearchTree()
+        {
+        }
+
+        public void Insert(T element)
+        {
+            this.root = this.Insert(element, this.root);
+        }
+
+        public bool Contains(T element)
+        {
+            Node current = this.FindElement(element);
+
+            return current != null;
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(this.root, action);
+        }
+
+        public IBinarySearchTree<T> Search(T element)
+        {
+            Node current = this.FindElement(element);
+
+            return new BinarySearchTree<T>(current);
+        }
+
+        public void Delete(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        //2 1 3 -> 1
+        public void DeleteMin()
+        {
+            if (this.root == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            this.DeleteMinRecursivly(this.root);
+        }
+
+        private void DeleteMinRecursivly(Node root)
+        {
+            if (this.root.Left == null)
+            {
+                this.root = null;
+                return;
+            }
+            else if (this.root.Left is null && this.root.Right is not null)
+            {
+
+            }
+            else
+            {
+                while (root != null)
+                {
+                    this.DeleteMinRecursivly(root.Left);
+                }
+
+                this.root.Left = null;
+            }
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Rank(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Select(int rank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Ceiling(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Floor(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            var list = new List<T>();
+            var current = this.root;
+
+            //while (current != null)
+            //{
+                
+            //    if (current.Value.CompareTo(startRange) > 0)
+            //    {
+
+            //    }
+            //    else if (current.Value.CompareTo(endRange) < 0)
+            //    {
+
+            //    }
+            //}
+
+            return list;
+        }
+
+
+        private Node FindElement(T element)
+        {
+            Node current = this.root;
+
+            while (current != null)
+            {
+                if (current.Value.CompareTo(element) > 0)
+                {
+                    current = current.Left;
+                }
+                else if (current.Value.CompareTo(element) < 0)
+                {
+                    current = current.Right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.Insert(node.Value);
+            this.PreOrderCopy(node.Left);
+            this.PreOrderCopy(node.Right);
+        }
+
+        private Node Insert(T element, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(element);
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(element, node.Left);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Insert(element, node.Right);
+            }
+
+            return node;
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+    }
+}
---
+++ Heaps-BST-Exercises/Playground/Program.cs
@@ -0,0 +1,24 @@
+﻿using _02.BinarySearchTree;
+using System;
+
+var  bst = new BinarySearchTree<int>();
+
+//bst.Insert(10);
+//bst.Insert(5);
+//bst.Insert(3);
+//bst.Insert(1);
+//bst.Insert(4);
+//bst.Insert(8);
+//bst.Insert(9);
+//bst.Insert(37);
+//bst.Insert(39);
+//bst.Insert(45);
+
+//bst.Range(37,4);
+
+bst.Insert(2);
+bst.Insert(1);
+bst.Insert(3);
+
+bst.DeleteMin();
+Console.WriteLine(bst);
