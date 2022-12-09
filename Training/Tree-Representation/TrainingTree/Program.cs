ddf9702df04a8e26bfb6390e4f1a02e7ae454ae1
mihail-karabashev <mihailkarabashev@gmail.com>
2022-12-09 17:09:15 +0200

    Add as string method to my tree implementation

---
+++ Training/Tree-Representation/TrainingTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tree;
+
+var tree = new Tree<int>(7,
+                     new Tree<int>(19,
+                        new Tree<int>(1),
+                        new Tree<int>(12),
+                        new Tree<int>(31)),
+                     new Tree<int>(21),
+                     new Tree<int>(14,
+                        new Tree<int>(23),
+                        new Tree<int>(6))
+                     );
+
+var ss = tree.AsString();
+
+Console.WriteLine(ss);
---
+++ Training/Tree-Representation/TrainingTree/Tree.cs
@@ -0,0 +1,89 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Metrics;
+    using System.Reflection;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> _children;
+
+        public Tree(T key)
+        {
+            Key = key;
+            _children = new List<Tree<T>>();
+        }
+
+        public Tree(T key, params Tree<T>[] children)
+            : this(key)
+        {
+            foreach (var child in children)
+            {
+                child.Parent = this;
+                _children.Add(child);
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => _children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AsString()
+        {
+            var sb = new StringBuilder();
+            int counter = 0;
+            AsStringWithDfs(this, sb, ref counter);
+            return sb.ToString();
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetDeepestKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AsStringWithDfs(Tree<T> tree, StringBuilder sb, ref int counter)
+        {
+            //   7
+            // 19
+            //1 12 31
+            sb.AppendLine(new string(' ', counter) +  tree.Key.ToString()); 
+
+            foreach (var child in tree.Children)
+            {
+                counter += 2;
+                AsStringWithDfs(child,sb, ref counter);
+            }
+
+            counter-=2;
+        }
+    }
+}
