60cb52b3f50fedb5fcbda0a403acbbce3cae303e
mihail-karabashev <mihailkarabashev@gmail.com>
2023-01-07 14:31:59 +0200

    Add solution for Increasing Order with BST
    LeetCode

---
+++ Training/LeetCode-Problems/IncreasingOrder/Program.cs
@@ -0,0 +1,10 @@
+﻿using RangeBST;
+
+var treeNode = new TreeNode(
+    new TreeNode(
+        new TreeNode(new TreeNode(null,null,1), null, 2), new TreeNode(null, null, 4), 3),
+    new TreeNode(null, new TreeNode(new TreeNode(null,null,7), new TreeNode(null,null,9), 8), 6),
+    5);
+
+var solution = new Solution();
+solution.IncreasingBST(treeNode);
--- Training/LeetCode-Problems/RangeBST/Solution.cs
+++ Training/LeetCode-Problems/RangeBST/Solution.cs
@@ -11,11 +11,40 @@ public class Solution
 
     private int RangeSumBST(TreeNode root, int low, int high, int sum)
     {
-        if (root._value >= low && root._value <= 15) sum += root._value;
+        if (root._value >= low && root._value <= high) sum += root._value;
 
         if (root._left != null) sum = RangeSumBST(root._left, low, high, sum);
         if (root._right != null) sum = RangeSumBST(root._right, low, high, sum);
 
         return sum;
+    }
+
+    public TreeNode IncreasingBST(TreeNode root)
+    {
+        var listNodes = new List<TreeNode>();
+        IncreasingBST(root, listNodes);
+
+        root = listNodes[0];
+        root._left = null;
+        root._right = listNodes[1];
+
+        for (int i = 1; i < listNodes.Count; i++)
+        {
+            listNodes[i]._left = null;
+
+            if (i < listNodes.Count - 1) listNodes[i]._right = listNodes[i + 1];
+            else listNodes[i]._right = null;
+        }
+
+        return root;
+    }
+
+    private void IncreasingBST(TreeNode node, List<TreeNode> listOfNodes)
+    {
+        if (node == null) return;
+
+        if (node._left != null) IncreasingBST(node._left, listOfNodes);
+         listOfNodes.Add(node);
+        if (node._right != null) IncreasingBST(node._right, listOfNodes);
     }
 }
